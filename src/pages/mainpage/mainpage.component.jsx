ea0e537ead2d85bf3321fec680023b2bc0038abd
Thuc Nguyen <thucdlnguyen@gmail.com>
2020-02-29 14:27:38 -0500

    Created a selling clothing prototype with react application. This is the main page

---
+++ src/pages/mainpage/mainpage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './mainpage.styles.scss';
+
+const MainPage = () => (
+  <div className='mainpage'>
+    <Directory />
+  </div>
+);
+
+export default MainPage;
