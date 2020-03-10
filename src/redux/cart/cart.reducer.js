9e8420991dd7e11a366f65337e5c91327e780d82
Thuc Nguyen <thucdlnguyen@gmail.com>
2020-03-10 17:41:45 -0400

    Appearing multiple items with quantity number in cart items reducer under cart utils.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
 
 import CartActionTypes from './cart.types';
 
+import { addItemToCart } from './cart.utils';
+
 const INITIAL_STATE = {
   hidden: true,
   cartItems: []
@@ -16,7 +18,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload]
+        cartItems: addItemToCart(state.cartItems, action.payload)
       };
     default:
       return state;
