7ca7ab6bdd143db64802b06de29475fad0de7e1d
Thuc Nguyen <thucdlnguyen@gmail.com>
2020-03-11 20:14:58 -0400

    Updating the check out page of the cart reducer to persist across sessions.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
-
 import './index.css';
 
 import App from './App';
 
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter> 
-      <App />
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
     document.getElementById('root')
