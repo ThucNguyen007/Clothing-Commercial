f80edbcc01a05a00e337e92c0f9842246a0f71b9
Thuc Nguyen <thucdlnguyen@gmail.com>
2020-03-14 07:12:59 -0400

    Updating the website with Stripe Integration to improve payment method.

---
+++ src/components/stripe-feature/stripe-feature.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeFeatureButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_xXLCTshes83fkjzlDWSez8D200ct4XR17R';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful!');
+    };
+    
+    return (
+        <StripeCheckout
+          label='Pay Now'
+          name='CRWN Clothing Ltd.'
+          billingAddress
+          shippingAddress
+          image='https://svgshare.com/i/CUz.svg'
+          description={`Your total is $${price}`}
+          amount={priceForStripe}
+          panelLabel='Pay Now'
+          token={onToken}
+          stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeFeatureButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeFeatureButton from '../../components/stripe-feature/stripe-feature.component';
+
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -30,6 +32,12 @@ const CheckoutPage = ({ cartItems, total }) => (
           <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className='total'>TOTAL: ${total} </div>
+        <div className='test-warning'>
+          *Please use the following test credit card for payments*
+          <br />
+          4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        <StripeFeatureButton price={total} />
     </div>
 );
 
